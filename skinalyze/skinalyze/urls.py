ace90902be8c0feb91d19b2994ee202b733e1fc4
AmruthaPaalathara01 <amruthadm2001@gmail.com>
2024-11-26 18:41:20 +0530

    added the overall page

--- skinalyze/skinalyze/urls.py
+++ skinalyze/skinalyze/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(''),
+    path('',home, name='home'),
+
 ]
